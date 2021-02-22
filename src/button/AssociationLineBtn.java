96799c52756574bb44406ecd8fbc56254133a1b6
Harry Tang <h125harry@gmail.com>
2021-02-22 10:20:09 +0800

    Update AssociationLineBtn.java

--- src/button/AssociationLineBtn.java
+++ src/button/AssociationLineBtn.java
@@ -4,7 +4,7 @@ import mode.AssociationLineMode;
 import mode.Mode;
 import mode.SelectMode;
 
-public class AssociationLineBtn extends Button{
+public class AssociationLineBtn extends Button {
 	private Mode associationLineMode = new AssociationLineMode();
 	
 	public AssociationLineBtn(String name) {

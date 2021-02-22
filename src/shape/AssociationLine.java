92e3f855427e0cb77eaf6794c4308352b799963d
Harry Tang <h125harry@gmail.com>
2021-02-22 10:25:13 +0800

    Update AssociationLine.java

--- src/shape/AssociationLine.java
+++ src/shape/AssociationLine.java
@@ -2,7 +2,7 @@ package shape;
 
 import java.awt.Graphics2D;
 
-public class AssociationLine extends BasicLine{
+public class AssociationLine extends BasicLine {
 	public AssociationLine(Port port) {
 		super(port);
 		// TODO Auto-generated constructor stub
